13b64fb86930aa7671430f435af1f359bd4dd0f0
Notarin Steele <424c414e4b@gmail.com>
2025-04-28 13:32:28 -0400

    Tiny whitespace fix

--- NixOS/host-configs/uriel/configuration.nix
+++ NixOS/host-configs/uriel/configuration.nix
@@ -10,7 +10,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
 
-  #Program configurations
+  # Program configurations
   programs = {
     hyprland.enable = true;
     steam.enable = true;
