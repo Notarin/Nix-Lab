2dc26829ab6b7e2dfca5023a92c0631e8683415a
Notarin Steele <424c414e4b@gmail.com>
2025-05-04 01:13:02 -0400

    Managed pubkeys with nixos

--- Users/kel.nix
+++ Users/kel.nix
@@ -8,5 +8,8 @@
     isNormalUser = true;
     hashedPasswordFile = "/persistent/passwords/kel";
     shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDnSAM81oiQ4iIWUl5Dc2iTbFebJXgwX/bepQTPK9NNq shiki@catbox"
+    ];
   };
 }
--- Users/notarin.nix
+++ Users/notarin.nix
@@ -9,5 +9,11 @@
     ];
     isNormalUser = true;
     hashedPasswordFile = "/persistent/passwords/notarin";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFU1sdK5nNgNhCFusMYY61FXMDD8+Hws+igIgAOb7xpg notarin@uriel"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFpUz7+oWy492j1T6es6274g3cADuQEuANx3lNEy6I7d notarin@gabriel"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAcxVq5W1Rs3Hk1sTvakwt/Hmug1gIkuo62yKwD0iMw/ JuiceSSH"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHQs62gxckea4wnA0DZI9nauzq7fh/nEgpRIMJiKJigjAAAADHNzaDp3b2dvLmRldg== notarin@arch"
+    ];
   };
 }
