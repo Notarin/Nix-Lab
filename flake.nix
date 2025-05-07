9c35458085a1131ca30a4bc798309794b590c02e
Notarin Steele <424c414e4b@gmail.com>
2025-05-07 02:59:55 -0400

    Implemented flakes for future use

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     impermanence.url = "github:nix-community/impermanence";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
@@ -9,43 +10,48 @@
       nixpkgs,
       self,
       impermanence,
+      flake-utils,
       ...
     }:
-    let
-      # All managed hosts start reverse SSH tunnels to the sshNetServer.
-      # This eliminates the need to expose every host to the internet.
-      # I call this the "SSH-Net".
-      sshNetServerHost = "wogo.dev";
-      sshNetPortIndex = 2000 + 1;
+    flake-utils.lib.eachDefaultSystemPassThrough (
+      system:
+      let
+        # All managed hosts start reverse SSH tunnels to the sshNetServer.
+        # This eliminates the need to expose every host to the internet.
+        # I call this the "SSH-Net".
+        sshNetServerHost = "wogo.dev";
+        sshNetPortIndex = 2000 + 1;
 
-      nixos_hosts = nixpkgs.lib.imap0 (idx: hostName: {
-        inherit hostName;
-        sshNetPort = idx + sshNetPortIndex;
-      }) (builtins.attrNames (builtins.readDir ./NixOS/host-configs));
-    in
-    {
-      nixosConfigurations = builtins.listToAttrs (
-        map (
-          host:
-          let
-            hostName = host.hostName;
-          in
-          {
-            name = hostName;
-            value = nixpkgs.lib.nixosSystem {
-              specialArgs = {
-                inherit hostName sshNetServerHost;
-                sshNetPort = host.sshNetPort;
-                rootDir = self;
+        nixos_hosts = nixpkgs.lib.imap0 (idx: hostName: {
+          inherit hostName;
+          sshNetPort = idx + sshNetPortIndex;
+        }) (builtins.attrNames (builtins.readDir ./NixOS/host-configs));
+
+      in
+      {
+        nixosConfigurations = builtins.listToAttrs (
+          map (
+            host:
+            let
+              hostName = host.hostName;
+            in
+            {
+              name = hostName;
+              value = nixpkgs.lib.nixosSystem {
+                specialArgs = {
+                  inherit hostName sshNetServerHost;
+                  sshNetPort = host.sshNetPort;
+                  rootDir = self;
+                };
+                modules = [
+                  ./NixOS/host-configs/${hostName}/configuration.nix
+                  ./NixOS/common/configuration.nix
+                  impermanence.nixosModules.impermanence
+                ];
               };
-              modules = [
-                ./NixOS/host-configs/${hostName}/configuration.nix
-                ./NixOS/common/configuration.nix
-                impermanence.nixosModules.impermanence
-              ];
-            };
-          }
-        ) nixos_hosts
-      );
-    };
+            }
+          ) nixos_hosts
+        );
+      }
+    );
 }
