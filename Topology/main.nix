9b02b554f79f7b5ab9c14d408655a32c3dec855a
Notarin Steele <424c414e4b@gmail.com>
2025-05-10 08:16:02 -0400

    Revised topology format

--- Topology/main.nix
+++ Topology/main.nix
@@ -1,8 +1,13 @@
 {config, ...}: {
   networks.home = {
-    name = "Home Network";
+    name = "Home LAN";
     cidrv4 = "192.168.86.0/24";
     cidrv6 = "2603:6081:8300:667b::/64";
+  };
+  networks.internet = {
+    name = "The Internet";
+    cidrv4 = "0.0.0.0/0";
+    cidrv6 = "::/0";
   };
   nodes = {
     router = {
@@ -23,7 +28,7 @@
         };
         wan = {
           addresses = ["174.111.31.103"];
-          network = "home";
+          network = "internet";
           type = "ethernet";
           physicalConnections = [
             {
@@ -40,7 +45,31 @@
       hardware.info = "Spectrum Modem";
       interfaces = {
         eth0 = {
+          network = "internet";
           type = "ethernet";
+        };
+        wan = {
+          network = "internet";
+          icon = ./assets/icons/coaxial.svg;
+          type = "coaxial";
+          physicalConnections = [
+            {
+              node = "isp";
+              interface = "*";
+            }
+          ];
+        };
+      };
+    };
+    isp = {
+      name = "ISP";
+      deviceType = "isp";
+      deviceIcon = config.icons.devices.cloud.file;
+      hardware.info = "Spectrum";
+      interfaces = {
+        "*" = {
+          icon = ./assets/icons/coaxial.svg;
+          network = "internet";
         };
       };
     };
