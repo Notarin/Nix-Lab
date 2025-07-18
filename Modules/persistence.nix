b197277ca16ea582bfb93264503e70629685b511
Notarin Steele <424c414e4b@gmail.com>
2025-07-18 10:33:05 -0400

    Added comments to persistence.nix
    Explained what files and dirs are persisted for.

--- Modules/persistence.nix
+++ Modules/persistence.nix
@@ -2,17 +2,18 @@
   environment.persistence."/persistent" = {
     hideMounts = true;
     files = [
-      "/etc/machine-id"
+      "/etc/machine-id" # Used for various inter-pc communication purposes
+      # All these keys are used to persist SSH identity
       "/etc/ssh/ssh_host_rsa_key"
       "/etc/ssh/ssh_host_rsa_key.pub"
       "/etc/ssh/ssh_host_ed25519_key"
       "/etc/ssh/ssh_host_ed25519_key.pub"
     ];
     directories = [
-      "/var/lib/nixos"
-      "/var/lib/iwd"
-      "/var/lib/bluetooth"
-      "/root"
+      "/var/lib/nixos" # Needed for uid persistence
+      "/var/lib/iwd" # Needed to keep wifi passwords and settings
+      "/var/lib/bluetooth" # Needed to keep bluetooth pairings
+      "/root" # Could probably be done better, but keeps the root user home
     ];
   };
 }
