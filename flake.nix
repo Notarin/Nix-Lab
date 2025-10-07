b6c6ef7c9f845086912648bd18ad9145adf6f1a4
Notarin Steele <424c414e4b@gmail.com>
2025-10-07 10:18:42 -0400

    Made @llakala happy

--- flake.nix
+++ flake.nix
@@ -2,89 +2,98 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     impermanence.url = "github:nix-community/impermanence";
-    flake-utils.url = "github:numtide/flake-utils";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    sops-nix.url = "github:Mic92/sops-nix";
-    nix-topology.url = "github:oddlama/nix-topology";
-    hayabusa.url = "github:Notarin/hayabusa";
-    snix-bot.url = "github:Notarin/Snix-Bot";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-topology = {
+      url = "github:oddlama/nix-topology";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hayabusa = {
+      url = "github:Notarin/hayabusa";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    snix-bot = {
+      url = "github:Notarin/Snix-Bot";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    SHID = {
+      url = "github:Notarin/SHID";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     self,
     impermanence,
-    flake-utils,
     treefmt-nix,
     sops-nix,
     nix-topology,
     hayabusa,
     snix-bot,
+    SHID,
     ...
-  }:
-    flake-utils.lib.eachDefaultSystemPassThrough (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            self.overlays.topology
-          ];
-        };
-        treefmt-config = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-
-        nixos_hosts = nixpkgs.lib.imap0 (idx: hostName: {
-          inherit hostName;
-        }) (builtins.attrNames (builtins.readDir ./NixOS/host-configs));
-      in {
-        formatter.${system} = treefmt-config.config.build.wrapper;
-        checks.${system}.formatting = treefmt-config.config.build.check self;
-        devShells.${system}.default = pkgs.mkShell {
-          shellHook = ''
-            oldHookDir=$(git config --local core.hooksPath)
-
-            if [ "$oldHookDir" != "$PWD/.githooks" ]; then
-              read -rp "Set git hooks to $PWD/.githooks? (y/n) " answer
-              if [ "$answer" = "y" ]; then
-                git config core.hooksPath "$PWD"/.githooks
-                echo "Set git hooks to $PWD/.githooks"
-              else
-                echo "Skipping git hooks setup"
-              fi
-            fi
-          '';
-        };
-        nixosConfigurations = builtins.listToAttrs (
-          map (
-            host: let
-              hostName = host.hostName;
-            in {
-              name = hostName;
-              value = nixpkgs.lib.nixosSystem {
-                specialArgs = {
-                  inherit self nixos_hosts hostName nix-topology snix-bot system;
-                  rootDir = self;
-                };
-                modules = [
-                  ./NixOS/host-configs/${hostName}/configuration.nix
-                  ./NixOS/common/configuration.nix
-                  impermanence.nixosModules.impermanence
-                  sops-nix.nixosModules.sops
-                  nix-topology.nixosModules.default
-                  hayabusa.nixosModules.default
-                ];
-              };
-            }
-          )
-          nixos_hosts
-        );
-        overlays.topology = nix-topology.overlays.default;
-        topology.${system} = import nix-topology {
-          inherit pkgs;
-          modules = [
-            ./Topology/main.nix
-            {nixosConfigurations = self.nixosConfigurations;}
-          ];
-        };
-      }
+  }: let
+    systems = ["x86_64-linux"];
+    buildEachSystem = output: builtins.map (system: output system) systems;
+    buildAllSystems = output: (
+      builtins.foldl' (acc: elem: nixpkgs.lib.recursiveUpdate acc elem) {} (buildEachSystem output)
     );
+  in (buildAllSystems (
+    system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          self.overlays.topology
+        ];
+      };
+      treefmt-config = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+
+      nixos_hosts = nixpkgs.lib.imap0 (idx: hostName: {
+        inherit hostName;
+      }) (builtins.attrNames (builtins.readDir ./NixOS/host-configs));
+    in {
+      formatter.${system} = treefmt-config.config.build.wrapper;
+      checks.${system}.formatting = treefmt-config.config.build.check self;
+      devShells.${system}.default = SHID.devShells.${system}.default;
+      nixosConfigurations = builtins.listToAttrs (
+        map (
+          host: let
+            hostName = host.hostName;
+          in {
+            name = hostName;
+            value = nixpkgs.lib.nixosSystem {
+              specialArgs = {
+                inherit self nixos_hosts hostName nix-topology snix-bot system;
+                rootDir = self;
+              };
+              modules = [
+                ./NixOS/host-configs/${hostName}/configuration.nix
+                ./NixOS/common/configuration.nix
+                impermanence.nixosModules.impermanence
+                sops-nix.nixosModules.sops
+                nix-topology.nixosModules.default
+                hayabusa.nixosModules.default
+              ];
+            };
+          }
+        )
+        nixos_hosts
+      );
+      overlays.topology = nix-topology.overlays.default;
+      topology.${system} = import nix-topology {
+        inherit pkgs;
+        modules = [
+          ./Topology/main.nix
+          {nixosConfigurations = self.nixosConfigurations;}
+        ];
+      };
+    }
+  ));
 }
