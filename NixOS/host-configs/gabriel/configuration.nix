03cc2a1838da9d87bb0cc474a79a425d6b360048
Notarin Steele <424c414e4b@gmail.com>
2025-11-29 14:30:08 -0500

    Revert "Added experimental daemon config to gabriel"
    This reverts commit 74afa81076a740b4ef7e985994de3eb58b842460.
    Possibly contributed to a soft-bricking issue.

--- NixOS/host-configs/gabriel/configuration.nix
+++ NixOS/host-configs/gabriel/configuration.nix
@@ -1,25 +1,10 @@
-{
-  lib,
-  rootDir,
-  ...
-}: {
+{rootDir, ...}: {
   imports = [
     ./hardware-configuration.nix
     ./topology.nix
     (rootDir + /Modules/persistence.nix)
     (rootDir + /Users/notarin.nix)
   ];
-
-  nix = {
-    daemonIOSchedClass = lib.mkDefault "idle";
-    daemonCPUSchedPolicy = lib.mkDefault "idle";
-  };
-  # put the service in top-level slice
-  # so that it's lower than system and user slice overall
-  # instead of only being lower in system slice
-  systemd.services.nix-daemon.serviceConfig.Slice = "-.slice";
-  # always use the daemon, even executed  with root
-  environment.variables.NIX_REMOTE = "daemon";
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
