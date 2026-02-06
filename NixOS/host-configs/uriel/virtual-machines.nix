fc619957fd8887df6a3bff0fa2941cc1d8ce0ec5
Notarin Steele <424c414e4b@gmail.com>
2026-02-06 04:55:07 -0500

    Added TPM support to VMs on Uriel

--- NixOS/host-configs/uriel/virtual-machines.nix
+++ NixOS/host-configs/uriel/virtual-machines.nix
@@ -1,6 +1,11 @@
 {
   # Enable libvirt VM management
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      swtpm.enable = true;
+    };
+  };
   # Adding my main user to the libvirt group
   users.users.notarin.extraGroups = ["libvirtd"];
 }
