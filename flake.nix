a3fed81f3ed7d13e69d08a8ccea985f9e53640df
Notarin Steele <424c414e4b@gmail.com>
2026-04-27 21:24:40 -0400

    Dropped SHID and the devshell
    Don't need them, it was silly and direnv can do the job just fine in a
    cleaner fashion.

--- flake.nix
+++ flake.nix
@@ -12,11 +12,6 @@
       inputs.treefmt-nix.follows = "";
       inputs.SHID.follows = "";
     };
-    SHID = {
-      url = "github:Notarin/SHID";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.treefmt-nix.follows = "";
-    };
     # Pinning my home-manager so I may follow this flake to its nixpkgs
     home-manager = {
       url = "github:Notarin/home-manager";
@@ -31,7 +26,6 @@
   outputs = {
     nixpkgs,
     self,
-    SHID,
     ...
   }: let
     systems = ["x86_64-linux"];
@@ -46,7 +40,6 @@
         packages.x86_64-linux.installer = self.nixosConfigurations.installer.config.system.build.isoImage;
         formatter.${system} = pkgs.callPackage ./formatter.nix {};
         checks.${system}.formatting = pkgs.callPackage ./formatter.nix {checkDir = self;};
-        devShells.${system}.default = SHID.devShells.${system}.default;
         nixosConfigurations = import ./NixOS {inherit self;};
       }
     );
