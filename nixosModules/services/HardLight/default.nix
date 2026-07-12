16276fd0c759db0a703641d4b68d418d68106bd0
Notarin Steele <424c414e4b@gmail.com>
2026-07-12 18:55:25 -0400

    Setup forgejo for HardLight

--- nixosModules/services/HardLight/default.nix
+++ nixosModules/services/HardLight/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./forgejo
     ./gerrit
     ./wiki
   ];
---
+++ nixosModules/services/HardLight/forgejo/default.nix
@@ -0,0 +1,21 @@
+{
+  services = {
+    forgejo = {
+      enable = true;
+      settings.server = {
+        ROOT_URL = "https://hl.squishcat.net/jo/";
+      };
+    };
+    nginx.virtualHosts."hl.squishcat.net" = {
+      locations."/jo/" = {
+        proxyPass = "http://localhost:3000/";
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+    };
+  };
+}
