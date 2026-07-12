902a984d23cc849b58a4136defef9e03aeb7258e
Notarin Steele <424c414e4b@gmail.com>
2026-07-12 18:53:22 -0400

    Migrated derivation for mdbook to its own repo

--- nixosModules/services/HardLight/wiki/default.nix
+++ nixosModules/services/HardLight/wiki/default.nix
@@ -1,7 +1,7 @@
-{pkgs, ...}: {
+{
   services.nginx.virtualHosts."hl.squishcat.net" = {
     locations."/wiki/" = {
-      alias = "${pkgs.callPackage ./package.nix {}}/";
+      alias = "${(builtins.getFlake "git+ssh://uriel.squishcat.net:23231/HardLight-Docs.git?rev=ff9cb7bb40ac2ddf74b8c9e3cb0b89a6a008e7f3").packages.x86_64-linux.default}/";
       index = "index.html";
       extraConfig = ''
         try_files $uri $uri/index.html /index.html;
